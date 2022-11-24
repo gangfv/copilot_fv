02d92ad7da7ab05fb04cf96c9b6433d82206cda8
0leja <granovoleg96614@gmail.com>
2022-11-24 20:43:21 +0300

    homepage created

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, CodePageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path("code", CodePageView.as_view(), name="code"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
 
+
+class CodePageView(TemplateView):
+    template_name = "pages/code.html"
+
