93d8a88f380925844f2cea5d835520126d3d63ad
gangfv <ganggstand@gmail.com>
2022-11-25 19:13:45 +0300

    add models prog_lang

--- api/models.py
+++ api/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
+PROGR_LAN = [
+    ('Python', 'Python'),
+    ('Kotlin', 'Kotlin'),
+    ('JavaScript', 'JavaScript'),
+]
+
 
 class Copilot(models.Model):
     id = models.AutoField(primary_key=True)
+    progr_lan = models.TextField(choices=PROGR_LAN, max_length=1000)
     textarea_old = models.TextField(max_length=1000, blank=True, null=True)
     textarea_new = models.TextField(max_length=1000, blank=True, null=True)
 
