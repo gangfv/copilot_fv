93b143c180b62b36e71de79864aa667966cbe34f
OlehGr <granof.web@gmail.com>
2022-11-25 13:57:55 +0300

    frontend version 1.0

--- static/js/codespace.js
+++ static/js/codespace.js
@@ -1,10 +1,12 @@
 const instance = axios.create({
-    baseURL: '../api/v1/',
+    baseURL: '../api/v1/docs/',
     'Accept': 'application/json',
     'Content-Type': 'application/json',
     'X-CSRFToken': Cookies.getCookies('csrftoken')
 })
 
+
+console.log(Cookies.getCookies('csrftoken'))
 
 
 const Code = {
@@ -13,19 +15,68 @@ const Code = {
         return {
             description: '',
             numbers: [1],
-            code: ''
+            code: '',
+
+            isLoading: false,
+            loadingTime: 15
         }
     },
     methods: {
         submitDescription(e) {
             e.preventDefault()
             //
-            console.log(this.description)
+            this.isLoading = true
+            this.startLoader()
+            console.log('Отправленное описание', this.description)
+
+            this.loadCode(this.description)
+
             //
             this.description = ''
         },
+        async loadCode(description) {
+            let newId = ''
+            await instance.post('copilot/', {
+                "textarea_old": description,
+                "textarea_new": ""
+            }).then(({data}) => {
+                newId = data.id
+            })
+
+            console.log(newId)
+
+            let newCode = ''
+
+            setTimeout(() => {
+                console.log('итерация')
+                instance.get(`copilot/${newId}/`).then(({data}) => {
+                    newCode = data.textarea_new
+                    console.log('Преобразованный код', newCode)
+                    this.setCode(newCode)
+
+                    this.isLoading = false
+
+                }, () => {
+                    this.isLoading = false
+                })
+            }, 15000)
+
+            return
+
+        },
         setCode(code) {
             this.code = code
+        },
+
+        startLoader() {
+            let loaderId = setInterval(() => {
+                this.loadingTime--
+            }, 1000)
+
+            setTimeout(() => {
+                clearInterval(loaderId)
+                this.loadingTime = 15
+            }, 15000)
         }
     },
     computed: {},
