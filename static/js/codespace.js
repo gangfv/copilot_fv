7b5969041393c7ad905ab17cedd77927438addb5
OlehGr <granof.web@gmail.com>
2022-11-25 10:47:31 +0300

    code page full finished

--- static/js/codespace.js
+++ static/js/codespace.js
@@ -1,34 +1,43 @@
+const instance = axios.create({
+    baseURL: '../api/v1/',
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'X-CSRFToken': Cookies.getCookies('csrftoken')
+})
+
+
+
 const Code = {
-  delimiters: ['[[', ']]'],
-  data() {
-    return {
-      description: '',
-      numbers: [1, 2],
-      code: ''
-    }
-  },
-  computed: {
-    rows() {
-       return `height: 1em * ${this.numbers.length}`
-    }
-  },
-  watch: {
-    description(value) {
-      console.log(value.split('\n'))
-      console.log(this.numbers)
-
-      if(value.split('\n').length > 2) {
-        let newNumbers = [1]
-        for (let i = 1; i < value.split('\n').length; i++) {
-          newNumbers.push(i+1)
+    delimiters: ['[[', ']]'],
+    data() {
+        return {
+            description: '',
+            numbers: [1],
+            code: ''
         }
-        this.numbers = newNumbers
-      } else {
-        return
-      }
-
+    },
+    methods: {
+        submitDescription(e) {
+            e.preventDefault()
+            //
+            console.log(this.description)
+            //
+            this.description = ''
+        },
+        setCode(code) {
+            this.code = code
+        }
+    },
+    computed: {},
+    watch: {
+        description(value) {
+            let newNumbers = [1]
+            for (let i = 1; i < value.split('\n').length; i++) {
+                newNumbers.push(i + 1)
+            }
+            this.numbers = newNumbers
+        }
     }
-  }
 }
 
 Vue.createApp(Code).mount('#app')
